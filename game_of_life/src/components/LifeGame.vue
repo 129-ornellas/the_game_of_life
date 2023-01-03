ac7b87ceb0ea67cc895f2586c068e56b8b7c5c83
129-ornellas <129.ornellas@gmail.com>
2023-01-03 14:31:56 -0300

    criando projeto

---
+++ game_of_life/src/components/LifeGame.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>LifeGame</h1>
+    <table class="center">
+      <tr v-for="linha in grid" :key="linha">
+        <td v-for="celula in linha" :key="celula"></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LifeGame',
+  data() {
+    return{
+      grid: [
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],
+        [0.0,0,0,0,0,0,0,0,0],]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table, th, td {
+  border: 1px solid black;
+}
+td{
+  width: 30px;
+  height: 30px;
+}
+
+.vivo{
+  background-color: blue;
+}
+</style>
